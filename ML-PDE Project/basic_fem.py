15b6248012ddef960cd064dbc9f31f13a49d3f31
Mostafa Faghih Shojaei <mfshojaei@gmail.com>
2023-11-19 23:28:12 -0500

    add files

---
+++ ML-PDE Project/basic_fem.py
@@ -0,0 +1,299 @@
+
+
+# ===========================================================
+# TO DO 0: 
+# Read and understand the following code 
+# and complete all TO DO boxes. 
+# ===========================================================
+
+import numpy as np
+
+def get_coordinate_rec_mesh(mesh_size):
+    """
+    This function takes mesh_size = (nx, ny) and generates 
+    the coordinates of nodes in a structured rectangular mesh
+    """
+    nx,ny = mesh_size
+    x,y = np.meshgrid(np.linspace(0, 1, nx),np.linspace(0, 1, ny))
+    # ===========================================================
+    # TO DO 1: 
+    # Make a 2D numpy array of size (nx*ny x 2) containing 
+    # x and y coordinates. This can be done in one line.
+    output = None
+    # ===========================================================
+    return output
+
+
+def get_connection_rec_mesh(mesh_size):
+    """
+    This function takes mesh_size = (nx, ny) and  generates 
+    the connectivity table ct for a structured rectangular mesh
+    using anticlockwise numbering
+    """
+    nx, ny  = mesh_size
+    local = np.array([0, 1, nx+1, nx])
+    ct = np.array([local + i+j*nx for j in range(ny-1) for i in range(nx-1)]).T
+    # ===========================================================
+    # TO DO 2: 
+    # Here ct is a 2D numpy array of size 4 x number of elements.
+    # Each column contains node numberings of each element.
+    # Looking at this function, what is the number of elements? 
+    # EXPLAIN your answer here as comments.
+    #
+    # ===========================================================
+    return ct
+
+
+def local_shape_functions(x,y):
+    """
+    This function takes coordinates x and y and returns 
+    shape function values and their derivatives in the 
+    reference element.
+    """
+    # ===========================================================
+    # TO DO 3: 
+    # Write shape functions and their corresponding derivatives.
+    # Must hold: b1(-1,-1) = b2(1,-1) = b3(1,1) = b4(-1,1) = 1.
+    b1 = None
+    b2 = None
+    b3 = None
+    b4 = None
+    ## ----------
+    bx1 = None
+    bx2 = None
+    bx3 = None
+    bx4 = None
+    ## ----------
+    by1 = None
+    by2 = None
+    by3 = None
+    by4 = None
+    # ===========================================================
+    b  = [b1, b2, b3, b4]
+    bx = [bx1, bx2, bx3, bx4]
+    by = [by1, by2, by3, by4]
+    b = np.array(b)
+    Gb = np.array([bx,by])  # Gb is gradient of b  
+    return b, Gb
+
+def gauss_int_points(ng=2):
+    """
+    This function takes number of Gaussian points ng
+    in each direction and returns Gaussian weights and points
+    """
+    if ng == 1:
+        return [0],[0],[2]
+    else:
+        if ng == 2:
+            # ===========================================================
+            # TO DO 4: 
+            # Write Gaussian weights and points for ng = 2.
+            xg = []
+            wg = []
+            # ===========================================================
+        elif ng == 3:
+            xg =[-np.sqrt(3/5), 0, +np.sqrt(3/5)]
+            wg = [5/9, 8/9, 5/9]
+        x,y = np.meshgrid(xg,xg)
+        w = np.array([wg]).T*wg
+        return x.flatten(), y.flatten(), w.flatten()
+
+
+def get_gauss_int(fun,ng):
+    """
+    This function takes a function fun(x,y) and 
+    number of Gaussian points ng and computes and returns 
+    the Gaussian integral of the function
+    """
+    xg,yg,wg= gauss_int_points(ng)
+    intg = 0
+    # ===========================================================
+    # TO DO 5: 
+    # Write ONE for loop to compute Gaussian integral
+    # using Gauss points xg, yg and Gauss weights wg.
+
+
+    # ===========================================================
+    return intg
+
+
+def get_element_jacobian(xe,ye):
+    """
+    This function takes the coordinates of 4 nodes (xe, ye) of any 
+    elements in the mesh and returns the Jacobian transformation 
+    matrix of that element.
+    """
+    # ===========================================================
+    # TO DO 6: 
+    # EXPLAIN why the following gives the Jacobin 
+    # transformation. What assumptions result in a constant 
+    # Jacobian matrix for all elements in the mesh?
+    #
+    # ===========================================================
+    return np.array([[(max(xe)-min(xe))/2,0],[0.0,(max(ye)-min(ye))/2]])
+
+
+def assemble_constant_mat(M,ct):
+    """
+    This function takes a matrix M and connectivity table ct
+    and returns an assembled global matrix 
+    """
+    n_local_dofs , n_elements = np.shape(ct)
+    a,b = np.shape(M)
+    assert(a==b)
+    assert(n_local_dofs==a)
+    total_dofs = np.max(np.max(ct))+1
+
+    assembled_mat = np.zeros((total_dofs,total_dofs))
+    # ===========================================================
+    # TO DO 7: 
+    # (a) EXPLAIN why the local matrix of each element
+    # is the same for all elements. Hint: there are 2 reasons.
+    # (b) Write ONE for loop to assemble the input local matrix M
+    # for all elements in the mesh using the connectivity table ct.
+
+
+    # ===========================================================
+    return assembled_mat
+
+
+def assemble_constant_vec(V,ct):
+    """
+    This function takes a vector V and connectivity table ct
+    and returns an assembled global vector 
+    """
+    n_local_dofs , n_elements = np.shape(ct)
+    a = np.shape(V)[0]
+    assert(n_local_dofs==a)
+    total_dofs = np.max(np.max(ct))+1
+    
+    assembled_vec = np.zeros(total_dofs)
+    # ===========================================================
+    # TO DO 8: 
+    # Write ONE for loop to assemble the input local vector V
+    # for all elements in the mesh using the connectivity table ct.
+
+
+    # ===========================================================
+    return assembled_vec
+
+
+"""
+--------------------------------------------
+We now use the above general FEM functions
+to solve a linear PDE in 2D.
+This example solves:
+div(grad(u)) + 1 = 0
+--------------------------------------------
+"""
+
+def get_K_and_F(m,n, ng=2):
+    """
+    This function takes mesh size m x n 
+    and the number of Gauss points ng and 
+    returns the global K and F for solving
+    div(grad(u)) + 1 = 0 such that KU = F
+    """
+    mesh_size  = (m,n)
+    total_dofs = mesh_size[0] * mesh_size[1]
+  
+    cord = get_coordinate_rec_mesh(mesh_size)
+    ct = get_connection_rec_mesh(mesh_size)
+    # ===========================================================
+    # TO DO 9: 
+    # EXPLAIN what happens in the following 2 lines.
+    #
+    # ===========================================================
+    xx = cord[0][ct]
+    yy = cord[1][ct]
+
+    J = get_element_jacobian(xx[:,0],yy[:,0])
+    invJ = np.linalg.inv(J)
+    detJ = np.linalg.det(J)
+
+    # ===========================================================
+    # TO DO 10: 
+    # Defie a function here called local_K that takes x,y as
+    # inputs and returns the local stiffness matrix of elements.
+    # Hint: the output should be a 4 x 4 numpy array.
+    # In Python, matrix multiplication is done by @, e.g., A @ B.
+    # Also see TO DO 11.
+
+
+
+
+    # ===========================================================
+
+    # ===========================================================
+    # TO DO 11: 
+    # EXPLAIN here the purpose of local_F(x,y) function 
+    # and how it works.
+    #
+    # ===========================================================
+    def local_F(x,y):
+        b =  local_shape_functions(x,y)[0]
+        return 1 * b * detJ
+    
+    # ===========================================================
+    # TO DO 12: 
+    # Write the entries/inputs of the following two functions.
+    Ke = get_gauss_int()
+    Fe = get_gauss_int()
+    # ===========================================================
+
+    # ===========================================================
+    # TO DO 13: 
+    # Use assemble_constant_mat() and assemble_constant_vec() 
+    # defined above to assemble Ke and Fe for all elements.
+    # Assembled Ke and Fe should be called K and F.
+
+
+    # ===========================================================
+    return K, F
+
+
+def solve(img):
+    """
+    This function takes a matrix img containing 
+    the boundary info and returns the solution of 
+    div(grad(u)) + 1 = 0 for the given boundary 
+    conditions.
+    """
+    m,n = img_shape = img.shape
+
+    K,F = get_K_and_F(m,n)
+
+    #boundary
+    dom_ind = (img == -2).flatten()
+    bc_ind1 = (img > 0).flatten()
+
+    Ud = img[img > 0]
+    
+    if bc_ind1.size > 0: 
+        F1 = K[:,bc_ind1] @ Ud
+
+    # ===========================================================
+    # TO DO 14: 
+    # Use dom_ind to remove rows and columns of K 
+    # corresponding to Dirichlet boundary conditions and
+    # call the resulting reduced matrix Kd.
+    # Hint: see Fd.
+
+
+    # ===========================================================
+
+    # ===========================================================
+    # TO DO 15: 
+    # EXPLAIN what is F1.
+    #
+    # ===========================================================
+    Fd = F[dom_ind] - F1[dom_ind]
+
+    # solve
+    U = np.zeros(m*n)
+    if bc_ind1.size > 0:
+        U[bc_ind1] = img.flatten()[bc_ind1]
+    U[dom_ind] = np.linalg.solve(Kd, Fd)
+
+    return U
+    
---
+++ ML-PDE Project/utilities.py
@@ -0,0 +1,105 @@
+"""
+Do NOT modify this file
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# def plot_mesh_as_image(img):
+  
+def plot_u(U):
+    m,n = U.shape
+    fig = plt.figure(figsize=(4,4))
+    ax = fig.add_subplot(1,1,1)
+    xp,yp = np.meshgrid(np.linspace(0,1,n),np.linspace(0,1,m))
+    up = np.reshape(U,(m,n))    
+    min_u = np.round(np.min(U),4)
+    max_u = np.round(np.max(U),4)
+    cp = ax.contourf(xp, yp, up)
+    plt.title('u(x,y), range = [' + str(min_u) + ', ' + str(max_u) + ']' )
+    ax.set_aspect('equal', adjustable='box')
+    cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])
+    plt.colorbar(cp, cax=cax) # Similar to fig.colorbar(im, cax = cax)
+    plt.show()
+
+def plot_results(img,U):
+    """
+    (1) plot mesh as an RGB image
+    """
+    d0 = img == 0
+    d1 = np.logical_and(img != -2, img !=0)  
+    m,n = img.shape
+    R = np.zeros((m,n))
+    G = np.zeros((m,n))
+    R[d0] = 1
+    G[d1] = 1
+    img = np.zeros((m,n,3))
+    img[:,:,0] = R
+    img[:,:,1] = G
+    img = img[::-1,:,:]
+
+    fig = plt.figure(figsize=(8,4))
+    ax = fig.add_subplot(1,2,1)
+    plt.imshow(img)
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Input image")
+    """
+    (2) plot U
+    """
+    xp,yp = np.meshgrid(np.linspace(0,1,n),np.linspace(0,1,m))
+    up = np.reshape(U,(m,n))    
+    ax = fig.add_subplot(1,2,2)
+    min_u = np.round(np.min(U),4)
+    max_u = np.round(np.max(U),4)
+    cp = ax.contourf(xp, yp, up)
+    plt.title('u(x,y), range = [' + str(min_u) + ', ' + str(max_u) + ']' )
+    ax.set_aspect('equal', adjustable='box')
+    cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])
+    plt.colorbar(cp, cax=cax) # Similar to fig.colorbar(im, cax = cax)
+    plt.show()
+
+
+def make_inpt_image(m,bc_value):
+  """
+  This function takes boundary values "bc_value = [bottom, top, left, right]"
+  of a square image of m x m and return the corresponding image matrix 
+  such that all domain pixels have value of -2.
+  """
+  img = np.zeros((m,m)) - 2
+  if bc_value[0] is not None: img[0, :] = bc_value[0]  #bottom
+  if bc_value[1] is not None: img[-1,:] = bc_value[1]  #top
+  if bc_value[2] is not None: img[:, 0] = bc_value[2]  #left
+  if bc_value[3] is not None: img[:,-1] = bc_value[3]  #right
+  return img
+
+
+def add_channel_to_one(img):
+    return np.reshape(img,(img.shape[0],img.shape[0],1))
+
+
+def repeat_one_image(img,n=1):
+    """
+    repeat a matrix n times along axis 0
+    use for data augmentation
+    """
+    return np.repeat([img],n,axis=0)
+
+
+class LayerFillRandomNumber(tf.keras.layers.Layer):
+    """ 
+    A customized Keras layer to insert uniform random values in (0,1) 
+    in image pixels if pixels value == -2.0
+    """
+    def __init__(self, name='fill-random-num'):
+        super(LayerFillRandomNumber, self).__init__(name=name)
+
+    def call(self, input):
+        output = input + tf.where(
+            input > -1.5, tf.fill(tf.shape(input), 0.0),
+            tf.random.uniform(tf.shape(input), minval=0.0, maxval=1.0)) + tf.where(
+                input > -1.5, tf.fill(tf.shape(input), 0.0),
+                tf.fill(tf.shape(input), 2.0))
+        return tf.cast(output,dtype=tf.double)
+
